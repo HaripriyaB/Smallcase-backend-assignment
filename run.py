df484e81c10e41831a702ac51749948a94b7368d
Haripriya Baskaran <haripriya.baskaran@thoughtworks.com>
2021-05-03 23:19:05 +0530

    Refactor run

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 from app import create_app
-config_name='development' # config_name = "development"
+config_name='production' # config_name = "development"
 app = create_app(config_name)
 
 if __name__ == '__main__':
