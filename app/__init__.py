c73742fa7d6ca931b4e04ec8c80f3f0424cba31a
Haripriya Baskaran <haripriya.baskaran@thoughtworks.com>
2021-05-04 14:57:28 +0530

    refactor run

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from os import abort
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
 from flask import request, jsonify
-from app.models import Portfolio, Trades
 
 # local import
 from instance.config import app_config
@@ -18,6 +17,9 @@ def create_app(config_name):
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
+    db.create_all()
+
+    from app.models import Portfolio, Trades
 
     @app.route('/test')
     def main():
