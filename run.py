991d977adc530d1b945473c18362b5a28ac0c359
Haripriya Baskaran <haripriya.baskaran@thoughtworks.com>
2021-05-04 14:20:42 +0530

    Add db create in run

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 import os
-from app import create_app
+from app import create_app,db
 config_name='production' # config_name = "development"
 app = create_app(config_name)
+db.create_all()
 
 if __name__ == '__main__':
     app.run()
