df97586cfacac4e8acad7f379ea19df459360df2
Haripriya Baskaran <haripriya.baskaran@thoughtworks.com>
2021-05-04 14:46:13 +0530

    remove db create in run

--- run.py
+++ run.py
@@ -2,7 +2,6 @@ import os
 from app import create_app,db
 config_name='production' # config_name = "development"
 app = create_app(config_name)
-db.create_all()
 
 if __name__ == '__main__':
     app.run()
